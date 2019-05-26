877ebde51b8665d1cf673013216ec9f11003972c
Emanuel Aguirre <ema_a_@hotmail.com>
2019-05-26 19:17:23 -0300

    completed lab2 setup

---
+++ lab2setup/DogTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class DogTest {    
+    @Test
+    public void testSmall() {
+        Dog d = new Dog(3);
+        assertEquals("yip", d.noise());
+    }
+
+    @Test
+    public void testLarge() {
+        Dog d = new Dog(20);
+        assertEquals("bark", d.noise());
+    }
+}
