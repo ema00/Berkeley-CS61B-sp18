17014a74570020cef50f7160d8eee857a98f2b8b
Emanuel Aguirre <ema_a_@hotmail.com>
2019-05-30 23:26:08 -0300

    added readPlanets to NBody class

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -9,7 +9,7 @@ import java.io.File;
 public class NBody {
 
     /**
-     *
+     * Reads the radius of the universe in the file containing data about the universe to be simulated.
      * @param filename is the file that contains data about the universe to be simulated.
      * @return the radius of the universe, in metres.
      */
@@ -22,4 +22,28 @@ public class NBody {
         return radius;
     }
 
+    /**
+     * Reads the planets in the file containing data about the universe to be simulated.
+     * @param filename is the file that contains data about the universe to be simulated.
+     * @return an array of planets in the file passed as parameter.
+     */
+    public static Planet[] readPlanets(String filename) {
+        File file = new File(filename);
+        In in = new In(file);
+        Planet[] planets;
+        int n = in.readInt();
+        planets = new Planet[n];
+        in.readDouble();
+        for (int i = 0; i < n; i++) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            double vx = in.readDouble();
+            double vy = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            planets[i] = new Planet(x, y, vx, vy, mass, imgFileName);
+        }
+        in.close();
+        return planets;
+    }
 }
