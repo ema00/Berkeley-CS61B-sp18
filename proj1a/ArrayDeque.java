1e4e053de627b52d92184676434b0b4fb3608af2
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-12 19:19:54 -0300

    added ArrayDeque.java to proj1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,55 @@
+/**
+ * ArrayDeque
+ * Double Ended Queue (deque) data structure.
+ * Implemented internally using an array.
+ * @param <T> is the type of objects to be stored in the deque.
+ */
+
+public class ArrayDeque<T> {
+
+    // TODO
+    public ArrayDeque() {
+
+    }
+
+    // TODO
+    public void addFirst(T item) {
+
+    }
+
+    // TODO
+    public void addLast(T item) {
+
+    }
+
+    // TODO
+    public boolean isEmpty() {
+
+    }
+
+    // TODO
+    public int size() {
+
+    }
+
+    // TODO
+    public void printDeque() {
+
+    }
+
+    // TODO
+    public T removeFirst() {
+
+    }
+
+    // TODO
+    public T removeLast() {
+
+    }
+
+    // TODO
+    public T get(int index) {
+
+    }
+
+}
