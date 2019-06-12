bc74388daf149190f8505b396fc2dc6f9d1273a2
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-11 22:26:11 -0300

    changed implementation comments

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,14 +1,14 @@
 /**
  * LinkedListDeque
  * Double Ended Queue (deque) data structure.
- * Implemented internally using a linked list
+ * Implemented internally using a doubly linked list.
  * @param <T> is the type of objects to be stored in the deque.
  */
 
 public class LinkedListDeque<T> {
 
     /**
-     * A node for the internal representation based on a linked list.
+     * A node for the internal representation based on a doubly linked list.
      * @param <T> is the type of objects to be stored in the deque.
      */
     private static class Node<T> {
