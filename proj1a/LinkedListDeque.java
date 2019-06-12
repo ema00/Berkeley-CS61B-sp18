addcf5b0bfc0ea0153d8d489819fa6ef20c070ba
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-11 22:48:12 -0300

    added methods to implement in LinkedListDeque.java for proj1a

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -12,7 +12,55 @@ public class LinkedListDeque<T> {
      * @param <T> is the type of objects to be stored in the deque.
      */
     private static class Node<T> {
+        T value;
+        Node<T> next;
+        Node<T> prev;
+    }
 
+
+    private Node<T> sentinel;
+    private int size;
+
+
+    public LinkedListDeque() {
+
+    }
+
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void printDeque() {
+        System.out.println("hello!!!");
+    }
+
+    public T removeFirst() {
+        return new Node<T>().value;
+    }
+
+    public T removeLast() {
+        return new Node<T>().value;
+    }
+
+    public T get(int index) {
+        return new Node<T>().value;
+    }
+
+    public T getRecursive(int index) {
+        return new Node<T>().value;
     }
 
 }
