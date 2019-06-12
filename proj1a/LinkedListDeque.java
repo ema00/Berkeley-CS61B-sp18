0da46e2c246dcf4eb093bf7e7eeb1f6dc16ca441
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-11 22:18:40 -0300

    added LinkedListDeque.java to proj1a

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,18 @@
+/**
+ * LinkedListDeque
+ * Double Ended Queue (deque) data structure.
+ * Implemented internally using a linked list
+ * @param <T> is the type of objects to be stored in the deque.
+ */
+
+public class LinkedListDeque<T> {
+
+    /**
+     * A node for the internal representation based on a linked list.
+     * @param <T> is the type of objects to be stored in the deque.
+     */
+    private static class Node<T> {
+
+    }
+
+}
