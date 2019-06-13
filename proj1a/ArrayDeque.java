128a72a68415e4196819596f8dc93aff00e995cb
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-13 19:02:58 -0300

    added addFirst, addLast and isEmpty to ArrayDeque in proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -25,19 +25,36 @@ public class ArrayDeque<T> {
         size = 0;
     }
 
-    // TODO
+    /**
+     * Adds an item at the beginning of the deque.
+     * @param item is the item to be added at the beginning of the deque.
+     */
     public void addFirst(T item) {
-
+        if (size == arr.length) {
+            changeCapacity(arr.length * UPSIZE_FACTOR);
+        }
+        arr[--start] = item;
+        size++;
     }
 
-    // TODO
+    /**
+     * Adds an item at the end of the deque.
+     * @param item is the item to be added at the end of the deque.
+     */
     public void addLast(T item) {
-
+        if (size == arr.length) {
+            changeCapacity(arr.length * UPSIZE_FACTOR);
+        }
+        arr[++start] = item;
+        size++;
     }
 
-    // TODO
+    /**
+     * Returns true or false whether the list is empty or not.
+     * @return true if the list is empty and false otherwise.
+     */
     public boolean isEmpty() {
-
+        return size == 0;
     }
 
     // TODO
@@ -65,9 +82,20 @@ public class ArrayDeque<T> {
 
     }
 
-    // TODO
-    private void increaseCapacity() {
-
+    /**
+     * Changes the size of the underlying array to the length passed as parameter.
+     * @param length is the new length of th underlying array.
+     */
+    private void changeCapacity(int length) {
+        T[] resized = (T[]) new Object[length];
+        for(i = 0; i < size; i++) {
+            int src = (start + i) % arr.length;
+            int dest = i;
+            resized[dest] = arr[src];
+        }
+        arr = resized;
+        start = 0;
+        end = size - 1;
     }
 
 }
