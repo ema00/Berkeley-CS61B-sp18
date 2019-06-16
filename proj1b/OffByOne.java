5668694a09e7f6b1e6be7834348b92995b7adad7
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-16 17:50:54 -0300

    added tests for OffByOne in proj1b

---
+++ proj1b/OffByOne.java
@@ -0,0 +1,12 @@
+/**
+ * OffByOne
+ */
+
+public class OffByOne implements CharacterComparator {
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        return false;
+    }
+
+}
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,11 +2,44 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    /*
+
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
 
-    // Your tests go here.
-    Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
+    @Test
+    public void testEqualCharsSameChars() {
+        char x = 'a';
+        char y = 'a';
+        assertFalse(offByOne.equalChars(x, y));
+    }
+
+    @Test
+    public void testEqualCharsDifferentChars1() {
+        char x = 'a';
+        char y = (char) (x + 2);
+        assertFalse(offByOne.equalChars(x, y));
+    }
+
+    @Test
+    public void testEqualCharsDifferentChars2() {
+        char x = 'a';
+        char y = (char) (x - 2);
+        assertFalse(offByOne.equalChars(x, y));
+    }
+
+    @Test
+    public void testEqualCharsEqualChars1() {
+        char x = 'a';
+        char y = (char) (x + 1);
+        assertTrue(offByOne.equalChars(x, y));
+    }
+
+    @Test
+    public void testEqualCharsEqualChars2() {
+        char x = 'a';
+        char y = (char) (x - 1);
+        assertTrue(offByOne.equalChars(x, y));
+    }
+
 }
