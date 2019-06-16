978d9fc2e3a8b792a491b9f8e7af74bbf5886acc
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-16 20:53:42 -0300

    added comments

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,10 +1,14 @@
 /**
  * OffByOne
+ * Compares characters for a difference of exactly one.
  */
 
 public class OffByOne implements CharacterComparator {
 
     @Override
+    /**
+     * Returns true for characters that are different by exactly one.
+     */
     public boolean equalChars(char x, char y) {
         return Math.abs(x - y) == 1;
     }
