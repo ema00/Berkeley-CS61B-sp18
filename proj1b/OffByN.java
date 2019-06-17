cc7689853c36384bc3d6211899ddb82e74dbcdea
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-16 21:45:48 -0300

    implemented OffByN in proj1b

---
+++ proj1b/OffByN.java
@@ -0,0 +1,29 @@
+/**
+ * OffByOne
+ * Compares characters for a difference of exactly N.
+ */
+
+public class OffByN implements CharacterComparator {
+
+    private int N;
+
+
+    public OffByN(int N) {
+        this.N = N;
+    }
+
+
+    /**
+     * Returns true for characters that are different by exactly one.
+     */
+    @Override
+    public boolean equalChars(char x, char y) {
+        if (('a' <= x && x <= 'z') && (('a' <= y && y <= 'z'))) {
+            return Math.abs(x - y) == N;
+        }
+        if (('A' <= x && x <= 'Z') && (('A' <= y && y <= 'Z'))) {
+            return Math.abs(x - y) == N;
+        }
+        return false;
+    }
+}
