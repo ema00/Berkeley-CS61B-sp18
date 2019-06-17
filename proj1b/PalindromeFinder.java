fb579a696520b70671add4d803c84e738e8d92d2
Emanuel Aguirre <ema_a_@hotmail.com>
2019-06-17 15:22:55 -0300

    modified PalindromeFinder to find also Off-by-4 palindromes, in proj1b

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -5,10 +5,23 @@ public class PalindromeFinder {
         int minLength = 4;
         In in = new In("../library-sp18/data/words.txt");
         Palindrome palindrome = new Palindrome();
+        CharacterComparator offBy4CC = new OffByN(4);             // CC to find Off-by-4 palindromes
 
+        System.out.println("\nStandard palindromes:");
         while (!in.isEmpty()) {
             String word = in.readString();
             if (word.length() >= minLength && palindrome.isPalindrome(word)) {
+                System.out.println(word);
+            }
+        }
+
+        in.close();
+        in = new In("../library-sp18/data/words.txt");
+
+        System.out.println("\nOff-by-4 palindromes:");
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            if (word.length() >= minLength && palindrome.isPalindrome(word, offBy4CC)) {
                 System.out.println(word);
             }
         }
