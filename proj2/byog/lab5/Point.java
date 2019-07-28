afb5c2c7a372a60746bdee051e52bdde65620a75
Emanuel Aguirre <ema_a_@hotmail.com>
2019-07-27 21:16:00 -0300

    lab5, added Point class

---
+++ proj2/byog/lab5/Point.java
@@ -0,0 +1,17 @@
+package byog.lab5;
+
+/**
+ * Represents a point coordinate in a plane, consisting of two coordinates: x and y.
+ * The class is immutable.
+ */
+public class Point {
+
+    public final int x;
+    public final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
