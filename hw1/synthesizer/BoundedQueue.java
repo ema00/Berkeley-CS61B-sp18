aa97d552edc9ba2420f78ac0ff15c3e9c5cd8f46
Emanuel Aguirre <ema_a_@hotmail.com>
2019-07-28 23:45:08 -0300

    hw1, added BondedQueue interface

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,38 @@
+package synthesizer;
+
+
+/**
+ * BoundedQueue
+ * Interface for a Queue that has a fixed capacity.
+ * Items can only be enqueued at the back of the Queue.
+ * Items can only be dequeued at the front of the Queue.
+ * No item is allowed to be enqueued if the Queue is full.
+ */
+public interface BoundedQueue<T> {
+
+    /* Return the size of the Queue */
+    int capacity();
+
+    /* Return the number of items currently in the Queue */
+    int fillCount();
+
+    /* Add item x to the back of the Queue */
+    void enqueue(T x);
+
+    /* Remove and return the front item from the Queue */
+    T dequeue();
+
+    /* Return (without removing) the front item of the Queue */
+    T peek();
+
+    /* Return whether the Queue is empty or not */
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    /* Return whether the Queue is full or not */
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
+
+}
