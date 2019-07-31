1fb24ef0c945f3204ae7beba11238a077be1679e
Emanuel Aguirre <ema_a_@hotmail.com>
2019-07-31 18:51:19 -0300

    hw1, uncommented tests for GuitarString

--- hw1/synthesizer/TestGuitarString.java
+++ hw1/synthesizer/TestGuitarString.java
@@ -12,7 +12,8 @@ import static org.junit.Assert.*;
  */
 
 public class TestGuitarString {
-/*    @Test
+
+    @Test
     public void testPluckTheAString() {
         double CONCERT_A = 440.0;
         GuitarString aString = new GuitarString(CONCERT_A);
@@ -21,9 +22,9 @@ public class TestGuitarString {
             StdAudio.play(aString.sample());
             aString.tic();
         }
-    }*/
+    }
 
-/*
+
     @Test
     public void testTic() {
         // Create a GuitarString of frequency 11025, which
@@ -52,10 +53,11 @@ public class TestGuitarString {
         assertEquals(expected, s5, 0.001);
 
     }
-*/
+
 
     /** Calls tests for GuitarString. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestGuitarString.class);
     }
+
 } 
