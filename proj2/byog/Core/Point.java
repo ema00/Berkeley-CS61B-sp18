d77273529f3dccf86a7a14f463e93074477fee92
Emanuel Aguirre <ema_a_@hotmail.com>
2019-08-22 21:29:40 -0300

    proj1, phase1, added Point class

---
+++ proj2/byog/Core/Point.java
@@ -0,0 +1,31 @@
+package byog.Core;
+
+/**
+ * Represents a point coordinate in a plane, consisting of two coordinates: x and y.
+ * The class is immutable.
+ */
+public class Point {
+
+    public final int x;
+    public final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns true if both Point instances are aligned on the x (horizontal) axis.
+     */
+    public static boolean alignedOnX(Point p1, Point p2) {
+        return p1.x == p2.x;
+    }
+
+    /**
+     * Returns true if both Point instances are aligned on the y (vertical) axis.
+     */
+    public static boolean alignedOnY(Point p1, Point p2) {
+        return p1.y == p2.y;
+    }
+
+}
