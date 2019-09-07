17e9ffce1a5e8b51e34b71dfbfa1775ebacf8863
Emanuel Aguirre <ema_a_@hotmail.com>
2019-09-07 17:32:57 -0300

    proj2, phase 1, fixed style issues

--- proj2/byog/Core/GameState.java
+++ proj2/byog/Core/GameState.java
@@ -1,7 +1,14 @@
 package byog.Core;
 
-import java.io.*;
 import java.util.List;
+import java.io.Serializable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import byog.TileEngine.TETile;
 
 
