669a5304dfa85479a1625748153ee7b8de82aefd
Emanuel Aguirre <ema_a_@hotmail.com>
2019-10-05 21:00:54 -0300

    proj06 extra (disjoint sets), created skeleton for disjoint sets implementation as weighted quick union

---
+++ lab6_disjoint-sets/weighted-quick-union/src/Main.java
@@ -0,0 +1,10 @@
+// Main method for running Disjoint Sets (Union Find) implemented as Weighted Quick Union
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+}
---
+++ lab6_disjoint-sets/weighted-quick-union/src/WeightedQuickUnionFind.java
@@ -0,0 +1,28 @@
+
+
+
+public class WeightedQuickUnionFind {
+
+    public WeightedQuickUnionFind(int n) {
+    }
+
+
+    public void validate(int v1) {
+    }
+
+    public int sizeOf(int v1) {
+    }
+
+    public int parent(int v1) {
+    }
+
+    public boolean connected(int v1, int v2) {
+    }
+
+    public void union(int v1, int v2) {
+    }
+
+    public int find(int v1) {
+    }
+
+}
