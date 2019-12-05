6be42dbe675c385797896c7b780234b1b5baf032
Emanuel Aguirre <ema_a_@hotmail.com>
2019-12-04 23:43:01 -0300

    hw2, added first implementation of Percolation class

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,141 @@
+/*
+ * CS 61B - Spring 2018
+ * Homework 2: Percolation
+ * https://sp18.datastructur.es/materials/hw/hw2/hw2
+ */
+
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
 public class Percolation {
+
+    private final int size;
+    private int numberOpenSites = 0;
+    private final Site[][] sites;
+    private final Map<Integer, Set<Site>> openSites;
+
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Grid size must be a natural number.");
+        }
+        size = N;
+        sites = Site.getGridOfSites(N);
+        openSites = new TreeMap<Integer, Set<Site>>();
+    }
+
+
+    /**
+     * Open the site (row, col) if it is not open already.
+     */
+    public void open(int row, int col) {
+        checkBounds(row, col);
+        Site site = sites[row][col];
+        site.open();
+        numberOpenSites += 1;
+        connectToOpenSites(site);
+    }
+
+    /**
+     * Is the site (row, col) open?
+     */
+    public boolean isOpen(int row, int col) {
+        checkBounds(row, col);
+        Site site = sites[row][col];
+        return site.isOpen();
+    }
+
+    /**
+     * Is the site (row, col) full?
+     * First, find the set to which the Site belongs to.
+     * Then, find if any of the sites of the top row belongs to the same set.
+     */
+    public boolean isFull(int row, int col) {
+        checkBounds(row, col);
+        Site site = sites[row][col];
+        Site[] topRowSites = sites[0];
+        Set<Site> openSiteSet = null;
+        for (int osKey : openSites.keySet()) {
+            Set<Site> openSite = openSites.get(osKey);
+            if (openSite.contains(site)) {
+                openSiteSet = openSite;
+                break;
+            }
+        }
+        if (openSiteSet != null) {
+            for (int column = 0; column < size; column++) {
+                if (openSiteSet.contains(topRowSites[column])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Number of open sites.
+     */
+    public int numberOfOpenSites() {
+        return numberOpenSites;
+    }
+
+    /**
+     * Does the system percolate?
+     */
+    public boolean percolates() {
+        for (int column = 0; column < size; column++) {
+            if (isFull(size - 1, column)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if site at row, col is within bounds of the grid.
+     */
+    private void checkBounds(int row, int col) {
+        if (row < 0 || col < 0 || size <= row || size <= col) {
+            throw new IndexOutOfBoundsException("row or column are out of bounds.");
+        }
+    }
+
+    /**
+     * Connects an open site to any neighboring open sites.
+     * First, creates a new full site comprised by the new node only.
+     * Second, merges full sites which are connected by the newly open site into the new full site.
+     * And deletes the previously unconnected sites, if any.
+     * Precondition: the site must be open just before calling this method.
+     */
+    private void connectToOpenSites(Site site) {
+        int newOpenSiteKey = numberOfOpenSites();
+        Set<Site> newOpenSite = new TreeSet<>();
+        Set<Site> neighbors = site.getNeighbors();
+
+        for (Site neighbor : neighbors) {
+            for (int osKey : openSites.keySet()) {
+                Set<Site> openSite = openSites.get(osKey);
+                if (openSite.contains(neighbor)) {
+                    newOpenSite.addAll(openSite);
+                    openSites.remove(osKey);
+                }
+            }
+        }
+
+        openSites.put(newOpenSiteKey, newOpenSite);
+    }
+
+
+    // use for unit testing (not required)
+    public static void main(String[] args) {
+    }
 
 }
---
+++ hw2/hw2/Site.java
@@ -0,0 +1,104 @@
+package hw2;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+
+
+/**
+ * Site
+ * Represents a site in a grid of sites, in the context of the percolation theory.
+ * Provides methods to generate a grid of (NxN) sites, and to get the neighbors of a site.
+ * Also implements methods for total ordering of the instances.
+ * This class serves to be used exclusively by the Percolation class.
+ */
+class Site implements Comparable<Site> {
+
+    final int id;
+    private boolean open;
+    private Set<Site> neighbors;    // above, below, and on both sides
+
+
+    private Site(int id) {
+        this.id = id;
+        this.open = false;
+        neighbors = new TreeSet<>();
+    }
+
+
+    boolean isOpen() {
+        return open;
+    }
+
+    void open() {
+        this.open = true;
+    }
+
+    /**
+     * Constructs and initializes a grid of N by N Sites.
+     */
+    static Site[][] getGridOfSites(int size) {
+        final Site[][] grid = new Site[size][size];
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                int siteId = row * size + col;
+                grid[row][col] = new Site(siteId);
+            }
+        }
+        generateNeighbors(grid);
+        return grid;
+    }
+
+    /**
+     * Generates the neighbors for each Site of a grid of Site instances.
+     * For this domain, neighbors are located above, below, and at both sides.
+     */
+    private static void generateNeighbors(Site[][] sites) {
+        int size = sites.length;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                Site site = sites[row][col];
+                if (row - 1 >= 0) {
+                    site.addNeighbor(sites[row - 1][col]);
+                }
+                if (row + 1 < size) {
+                    site.addNeighbor(sites[row + 1][col]);
+                }
+                if (col - 1 >= 0) {
+                    site.addNeighbor(sites[row][col - 1]);
+                }
+                if (col + 1 < size) {
+                    site.addNeighbor(sites[row][col + 1]);
+                }
+            }
+        }
+    }
+
+    private void addNeighbor(Site neighbor) {
+        if (neighbors.size() > 3) {
+            throw new RuntimeException("Number of neighbors for Site can't be > 4.");
+        }
+        neighbors.add(neighbor);
+    }
+
+    Set<Site> getNeighbors() {
+        return neighbors;
+    }
+
+    @Override
+    public int compareTo(Site other) {
+        if (other == null) {
+            throw new NullPointerException("Can not compare object of type Site to null.");
+        }
+        return this.id - other.id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o.getClass() != Site.class) {
+            throw new ClassCastException("Object to compare is not of type Site.");
+        }
+        return ((Site) o).id == this.id;
+    }
+
+}
