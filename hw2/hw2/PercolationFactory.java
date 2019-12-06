0cdd6be30f86d158383fd937f0a996e01311a7cb
Emanuel Aguirre <ema_a_@hotmail.com>
2019-12-05 23:37:41 -0300

    hw2, added PercolationStats class

--- hw2/hw2/PercolationFactory.java
+++ hw2/hw2/PercolationFactory.java
@@ -1,7 +1,11 @@
 package hw2;
 
+
+
 public class PercolationFactory {
+
     public Percolation make(int N) {
         return new Percolation(N);
     }
+
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,107 @@
 package hw2;
 
+import static java.lang.Math.pow;
+import java.time.Instant;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+
+
+/**
+ * PercolationStats
+ * Estimates the percolation threshold using the Montecarlo Method.
+ */
 public class PercolationStats {
+
+    private final int N;
+    private final int T;
+    private final double[] thresholds;
+    private final PercolationFactory pf;
+
+
+    /**
+     * Perform T independent experiments on an N-by-N grid.
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Grid size must be a natural number.");
+        }
+        this.N = N;
+        this.T = T;
+        this.thresholds = new double[T];
+        this.pf = pf;
+        StdRandom.setSeed(Instant.now().toEpochMilli());
+        simulatePercolation();
+    }
+
+
+    /**
+     * Sample mean of percolation threshold.
+     * mu = (x1 + x2 + ... + xT) / T
+     */
+    public double mean() {
+        double sumOfThresholds = 0;
+        for (int i = 0; i < T; i++) {
+            sumOfThresholds += thresholds[i];
+        }
+        return sumOfThresholds / T;
+    }
+
+    /**
+     * Sample standard deviation of percolation threshold.
+     * sigma^2 = [(x1 - mu)^2 + (x2 - mu)^2 + ... + (xT - mu)^2] / (T - 1)
+     */
+    public double stddev() {
+        double sumOfStandardDeviations = 0;
+        double mean = mean();
+        for (int i = 0; i < T; i++) {
+            sumOfStandardDeviations += pow(thresholds[i] - mean, 2);
+        }
+        return sumOfStandardDeviations / (T - 1);
+    }
+
+    /**
+     * Low endpoint of 95% confidence interval.
+     * mu - (1.96 * sigma) / [T^(1/2)]
+     */
+    public double confidenceLow() {
+        double mean = mean();
+        double variance = pow(stddev(), 0.5f);
+        return mean - 1.96 * variance / pow(T, 0.5f);
+    }
+
+    /**
+     * High endpoint of 95% confidence interval.
+     * mu + (1.96 * sigma) / [T^(1/2)]
+     */
+    public double confidenceHigh() {
+        double mean = mean();
+        double variance = pow(stddev(), 0.5f);
+        return mean + 1.96 * variance / pow(T, 0.5f);
+    }
+
+    private void simulatePercolation() {
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            int numOpenSites = calculateNumberOfSitesToPercolate(p);
+            int totalSites = N * N;
+            double threshold = ((double) numOpenSites) / totalSites;
+            thresholds[i] = threshold;
+        }
+    }
+
+    /**
+     * Opens sites one by one until the medium percolates.
+     */
+    private int calculateNumberOfSitesToPercolate(Percolation p) {
+        while (!p.percolates()) {
+            int row = StdRandom.uniform(N);
+            int col = StdRandom.uniform(N);
+            if (!p.isOpen(row, col)) {
+                p.open(row, col);
+            }
+        }
+        return p.numberOfOpenSites();
+    }
 
 }
