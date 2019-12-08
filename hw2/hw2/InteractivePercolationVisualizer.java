7722322caec5987cca57c7c186ba393129fb9589
Emanuel Aguirre <ema_a_@hotmail.com>
2019-12-08 12:47:40 -0300

    hw2, uncommented Percolation Visualizers, for debugging

--- hw2/hw2/InteractivePercolationVisualizer.java
+++ hw2/hw2/InteractivePercolationVisualizer.java
@@ -10,13 +10,17 @@
  *
  ******************************************************************************/
 package hw2;
+
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+
+
 public class InteractivePercolationVisualizer {
-    /*private static final int DELAY = 20;
+    private static final int DELAY = 20;
 
     public static void main(String[] args) {
+
         // N-by-N percolation system (read from command-line, default = 10)
         int N = 10;
         if (args.length == 1) {
@@ -58,5 +62,6 @@ public class InteractivePercolationVisualizer {
             }
             StdDraw.show(DELAY);
         }
-    }*/
+    }
+
 }
