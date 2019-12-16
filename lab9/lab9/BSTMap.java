f0561fd43d91cc10eebddb24670a32ec7234b8cb
Emanuel Aguirre <ema_a_@hotmail.com>
2019-12-16 18:33:37 -0300

    lab9, implemented get() and put() in BSTMap class

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -3,10 +3,12 @@ package lab9;
 import java.util.Iterator;
 import java.util.Set;
 
+
+
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author Emanuel Aguirre
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -25,13 +27,18 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    private Node root;  /* Root node of the tree. */
-    private int size; /* The number of key-value pairs in the tree */
+
+    /* Root node of the tree. */
+    private Node root;
+    /* The number of key-value pairs in the tree */
+    private int size;
+
 
     /* Creates an empty BSTMap. */
     public BSTMap() {
         this.clear();
     }
+
 
     /* Removes all of the mappings from this map. */
     @Override
@@ -44,22 +51,48 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        }
+        if (key.equals(p.key)) {
+            return p.value;
+        }
+
+        if (key.compareTo(p.key) < 0) {
+            return getHelper(key, p.left);
+        } else {
+            return getHelper(key, p.right);
+        }
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+     *  Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            size += 1;
+            return new Node(key, value);
+        }
+        if (key.equals(p.key)) {
+            p.value = value;
+            return p;
+        }
+
+        if (key.compareTo(p.key) < 0) {
+            p.left = putHelper(key, value, p.left);
+            return p;
+        } else {
+            p.right = putHelper(key, value, p.right);
+            return p;
+        }
     }
 
     /** Inserts the key KEY
@@ -67,13 +100,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -106,4 +139,5 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
     }
+
 }
