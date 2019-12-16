4d701c6c03b2a90561b770a907ea8a74b5c879c2
Emanuel Aguirre <ema_a_@hotmail.com>
2019-12-16 20:19:26 -0300

    lab9, implemented remove() and keySet() in MyHashMap class, and its tests

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -80,7 +81,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keySet = new HashSet<>();
+        for (ArrayMap<K,V> bucket : buckets) {
+            keySet.addAll(bucket.keySet());
+        }
+        return keySet;
     }
 
     /* Removes the mapping for the specified key from this map if exists.
@@ -88,7 +93,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        int hash = hash(key);
+        return buckets[hash].remove(key);
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -96,7 +102,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (get(key).equals(value)) {
+            return remove(key);
+        } else {
+            return null;
+        }
     }
 
     @Override
