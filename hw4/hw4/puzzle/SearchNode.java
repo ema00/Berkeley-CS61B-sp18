ec5fe32d1601ded7a1970f06b1933ab80794108d
Emanuel Aguirre <ema_a_@hotmail.com>
2020-01-07 18:42:00 -0300

    hw4, changed implementation of SearchNode class

--- hw4/hw4/puzzle/SearchNode.java
+++ hw4/hw4/puzzle/SearchNode.java
@@ -1,15 +1,17 @@
 package hw4.puzzle;
 
+import java.util.Comparator;
+
 
 
 /**
  * SearchNode
+ *
  * Represents one “move sequence” in the Search algorithm implemented by the Solver class.
+ * That is, the WorldState derived from the start state, and all the previous states that
+ * led from start to the current state.
  *
- * Consists of the current world state and a link to the previous world state.
- * Also has the number of moves from the start node up to this node.
- *
- * Each node in the graph is represented by a WorldState instance.
+ * Each node in the graph represents a WorldState instance.
  *
  * @author Emanuel Aguirre
  */
@@ -18,15 +20,36 @@ public class SearchNode {
     /* Current node in the graph. */
     private final WorldState state;
     /* Previous node in the graph. */
-    private final SearchNode prevNode;
+    private final SearchNode previousNode;
     /* Number of nodes traversed from the start node up to this node. */
     private final int moves;
 
 
-    public SearchNode(WorldState state, SearchNode prevNode) {
+    public SearchNode(WorldState state, SearchNode previousNode) {
         this.state = state;
-        this.prevNode = prevNode;
-        this.moves = prevNode.moves + 1;
+        this.previousNode = previousNode;
+        this.moves = previousNode == null ? 0 : previousNode.moves + 1;
+    }
+
+
+    public WorldState state() {
+        return state;
+    }
+
+    public SearchNode previousNode() {
+        return previousNode;
+    }
+
+    public int distanceFromStart() {
+        return moves;
+    }
+
+    public int estimatedDistanceToGoal() {
+        return state.estimatedDistanceToGoal();
+    }
+
+    public static Comparator<SearchNode> getComparator() {
+        return new SearchNodeComparator();
     }
 
 }
