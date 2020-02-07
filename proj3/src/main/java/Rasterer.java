47e58427727e3b103165509430de42fe402d164e
Emanuel Aguirre <ema_a_@hotmail.com>
2020-02-06 23:47:17 -0300

    proj3, implemented class Rasterer

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Math.abs;
 import static java.lang.Math.pow;
 
 
@@ -15,37 +16,39 @@ import static java.lang.Math.pow;
  * not draw the output correctly.
  * NOTE: (image) depth and zoom level are considered synonyms.
  *
- * @author: CS61B staff (interface of getMapRaster)
- * @author: Emanuel Aguirre (implementation)
+ * @author CS61B staff (interface of getMapRaster)
+ * @author Emanuel Aguirre (implementation)
  */
 public class Rasterer {
 
+    /*
+     * The root upper left/lower right longitudes and latitudes represent the bounding box of
+     * the root tile, as the images in the img/ folder are scraped.
+     * Longitude == x-axis; latitude == y-axis.
+     * (Can't make static import because MapServer is in default package).
+     */
+    private static final double ROOT_ULLON = MapServer.ROOT_ULLON;
+    private static final double ROOT_LRLON = MapServer.ROOT_LRLON;
+    private static final double ROOT_ULLAT = MapServer.ROOT_ULLAT;
+    private static final double ROOT_LRLAT = MapServer.ROOT_LRLAT;
+
+    /* Size of image tiles (can't make static import because MapServer is in default package). */
+    private static final int TILE_SIZE = MapServer.TILE_SIZE;
+
     /* Min and Max Zoom levels of the image tiles available to render an image. */
-    private static final int MIN_ZOOM_LEVEL = 0;
-    private static final int MAX_ZOOM_LEVEL = 7;
+    private static final int DEPTH_MIN = 0;
+    private static final int DEPTH_MAX = 7;
 
     /* Longitude Distance per Pixel (LonDPP) for each image depth (resolution) available. */
     private static final double[] LONDPP_AT_DEPTH = {
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 0)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 1)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 2)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 3)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 4)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 5)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 6)),
-        (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (MapServer.TILE_SIZE * pow(2, 7))
-    };
-
-    /* Number of tiles per axis (lon and lat) that comprise the bounding box at each zoom level. */
-    private static final int[] TILES_AT_ZOOM_LEVEL = {
-        (int) pow(2, 0),
-        (int) pow(2, 1),
-        (int) pow(2, 2),
-        (int) pow(2, 3),
-        (int) pow(2, 4),
-        (int) pow(2, 5),
-        (int) pow(2, 6),
-        (int) pow(2, 7),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 0)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 1)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 2)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 3)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 4)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 5)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 6)),
+        (ROOT_LRLON - ROOT_ULLON) / (TILE_SIZE * pow(2, 7))
     };
 
     /* Names of the query parameters. */
@@ -65,10 +68,11 @@ public class Rasterer {
     private static final String RESULT_DEPTH = "depth";
     private static final String RESULT_QUERY_SUCCESS = "query_success";
 
+    /* Flie format of the image files to use as tiles. */
+    private static final String IMAGE_FILE_FORMAT = ".png";
 
-    public Rasterer() {
-        // YOUR CODE HERE
-    }
+
+    public Rasterer() { }
 
 
     /**
@@ -102,6 +106,40 @@ public class Rasterer {
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         Map<String, Object> results = new HashMap<>();
 
+        double ulLon = params.get(QUERY_PARAM_ULLON);
+        double ulLat = params.get(QUERY_PARAM_ULLAT);
+        double lrLon = params.get(QUERY_PARAM_LRLON);
+        double lrLat = params.get(QUERY_PARAM_LRLAT);
+        double width = params.get(QUERY_PARAM_WIDTH);
+
+        double queryBoxLonDPP = lonDPP(ulLon, lrLon, (int) width);
+        int depth = selectDepth(queryBoxLonDPP);
+        boolean success = areValidCoordinates(ulLon, ulLat, lrLon, lrLat);
+
+        if (!success) {
+            results.put(RESULT_QUERY_SUCCESS, success);
+            results.put(RESULT_RENDER_GRID, null);
+            results.put(RESULT_ULLON, 0);
+            results.put(RESULT_ULLAT, 0);
+            results.put(RESULT_LRLON, 0);
+            results.put(RESULT_LRLAT, 0);
+            results.put(RESULT_DEPTH, depth);
+        } else {
+            String[][] renderGrid = selectRenderGrid(ulLon, ulLat, lrLon, lrLat, depth);
+            results.put(RESULT_QUERY_SUCCESS, success);
+            results.put(RESULT_RENDER_GRID, renderGrid);
+            results.put(RESULT_ULLON, ROOT_ULLON
+                    + xIndexLeft(ulLon, depth) * tileLonLength(depth));
+            results.put(RESULT_ULLAT, ROOT_LRLAT
+                    + (numTilesAtDepth(depth) - yIndexUpper(ulLat, depth))
+                    * tileLatLength(depth));
+            results.put(RESULT_LRLON, ROOT_ULLON
+                    + (xIndexRight(lrLon, depth) + 1) * tileLonLength(depth));
+            results.put(RESULT_LRLAT, ROOT_LRLAT
+                    + (numTilesAtDepth(depth) - (yIndexLower(lrLat, depth) + 1))
+                    * tileLatLength(depth));
+            results.put(RESULT_DEPTH, depth);
+        }
         return results;
     }
 
@@ -115,8 +153,96 @@ public class Rasterer {
      */
     public boolean areValidCoordinates(double ulLon, double ulLat, double lrLon, double lrLat) {
         return (ulLon < lrLon && lrLat < ulLat)
-                && (ulLon < MapServer.ROOT_LRLON || MapServer.ROOT_ULLON < lrLon)
-                && (MapServer.ROOT_LRLAT < ulLat || lrLat < MapServer.ROOT_ULLAT);
+                && (ulLon < ROOT_LRLON || ROOT_ULLON < lrLon)
+                && (ROOT_LRLAT < ulLat || lrLat < ROOT_ULLAT);
+    }
+
+    private String[][] selectRenderGrid(
+            double ulLon, double ulLat, double lrLon, double lrLat, int depth
+    ) {
+        int xLeft = xIndexLeft(ulLon, depth);
+        int xRight = xIndexRight(lrLon, depth);
+        int yUpper = yIndexUpper(ulLat, depth);
+        int yLower = yIndexLower(lrLat, depth);
+
+        int gridLengthX = xRight - xLeft + 1;
+        int gridLengthY = yLower - yUpper + 1;
+        String[][] grid = new String[gridLengthY][gridLengthX];     // [y][x]
+        for (int x = 0; x < gridLengthX; x++) {
+            for (int y = 0; y < gridLengthY; y++) {
+                int xTile = xLeft + x;
+                int yTile = yUpper + y;
+                grid[y][x] = createTileFileName(xTile, yTile, depth, IMAGE_FILE_FORMAT);
+            }
+        }
+        return grid;
+    }
+
+    /**
+     * For a grid of image tiles of depth D (which has 2^D x 2^D elements), returns the leftmost
+     * index in the X direction (longitude) that corresponds to the tile that contains the
+     * longitude coordinate passed as parameter.
+     * @param ulLon is the longitude coordinate of the upper left corner of the query box.
+     * @param depth is the image depth (zoom level) of the tiles on which to calculate the index.
+     * @return the leftmost index of an x-y grid of tiles, within the grid at the given depth.
+     */
+    private int xIndexLeft(double ulLon, int depth) {
+        double tileLongitudeLength = tileLonLength(depth);
+        return (ulLon - ROOT_ULLON) / tileLongitudeLength >= 0
+                ? (int) ((ulLon - ROOT_ULLON) / tileLongitudeLength)
+                : 0;
+    }
+
+    /**
+     * For a grid of image tiles of depth D (which has 2^D x 2^D elements), returns the rightmost
+     * index in the X direction (longitude) that corresponds to the tile that contains the
+     * longitude coordinate passed as parameter.
+     * @param lrLon is the longitude coordinate of the lower right corner of the query box.
+     * @param depth is the image depth (zoom level) of the tiles on which to calculate the index.
+     * @return the rightmost index of an x-y grid of tiles, within the grid at the given depth.
+     */
+    private int xIndexRight(double lrLon, int depth) {
+        double tileLongitudeLength = tileLonLength(depth);
+        return (lrLon - ROOT_ULLON) / tileLongitudeLength < numTilesAtDepth(depth)
+                ? (int) ((lrLon - ROOT_ULLON) / tileLongitudeLength)
+                : numTilesAtDepth(depth) - 1;
+    }
+
+    /**
+     * For a grid of image tiles of depth D (which has 2^D x 2^D elements), returns the topmost
+     * index in the Y direction (latitude) that corresponds to the tile that contains the
+     * latitude coordinate passed as parameter.
+     * @param ulLat is the latitude coordinate of the upper left corner of the query box.
+     * @param depth is the image depth (zoom level) of the tiles on which to calculate the index.
+     * @return the topmost index of an x-y grid of tiles, within the grid at the given depth.
+     */
+    private int yIndexUpper(double ulLat, int depth) {
+        double tileLatitudeLength = tileLatLength(depth);
+        return (ROOT_ULLAT - ulLat) / tileLatitudeLength >= 0
+                ? (int) ((ROOT_ULLAT - ulLat) / tileLatitudeLength)
+                : 0;
+    }
+
+    /**
+     * For a grid of image tiles of depth D (which has 2^D x 2^D elements), returns the bottommost
+     * index in the Y direction (latitude) that corresponds to the tile that contains the
+     * latitude coordinate passed as parameter.
+     * @param lrLat is the latitude coordinate of the lower right corner of the query box.
+     * @param depth is the image depth (zoom level) of the tiles on which to calculate the index.
+     * @return the bottommost index of an x-y grid of tiles, within the grid at the given depth.
+     */
+    private int yIndexLower(double lrLat, int depth) {
+        double tileLatitudeLength = tileLatLength(depth);
+        return (ROOT_ULLAT - lrLat) / tileLatitudeLength < numTilesAtDepth(depth)
+                ? (int) ((ROOT_ULLAT - lrLat) / tileLatitudeLength)
+                : numTilesAtDepth(depth) - 1;
+    }
+
+    /**
+     * Generate the filename for an image tile in an X, Y grid.
+     */
+    private String createTileFileName(int x, int y, int depth, String fileExt) {
+        return "d" + depth + "_x" + x + "_y" + y + fileExt;
     }
 
     /**
@@ -130,7 +256,7 @@ public class Rasterer {
      * @return the number of units of longitude per pixel, for the given parameters.
      */
     double lonDPP(double lon1, double lon2, int imageWidth) {
-        return Math.abs(lon2 - lon1) / imageWidth;
+        return abs(lon2 - lon1) / imageWidth;
     }
 
     /**
@@ -138,28 +264,56 @@ public class Rasterer {
      * The criteria for selecting LonDPP is: the greatest LonDPP that is less than or equal to
      * the LonDPP of the query box. If the LonDPP passed as parameter is off-bounds, then the
      * nearest LonDPP within bounds is returned.
-     * First, calculates the corresponding zoom level, according to the LonDPP values available in
-     * the array LON_DPP_AT_ZOOM (which contains the LonDPP available for the images available at
-     * each zoom level). The, based of the zoom level, return the corresponding LonDPP.
-     * @param lonDPP based on which to select the lonDPP according to the given criteria.
+     * First, calculates the corresponding depth (zoom level), according to LonDPP values available
+     * in the array LONDPP_AT_DEPTH (which contains the LonDPP available for the images available
+     * at each image depth). Then, based on the depth, returns the corresponding LonDPP.
+     * @param lonDPP based on which to select the lonDPP, according to the given criteria.
      * @return one of the fixed values for lonDPP, given by the image resolutions available.
      */
     double selectLonDPP(double lonDPP) {
-        int zoomLevel = selectZoomLevel(lonDPP);
-        return LONDPP_AT_DEPTH[zoomLevel];
+        int depth = selectDepth(lonDPP);
+        return LONDPP_AT_DEPTH[depth];
     }
 
     /**
-     * Selects the zoom level that corresponds to the LonDPP selected by the method selectLonDPP,
-     * using the same criteria as that method.
+     * Selects the image depth (zoom level) that corresponds to the LonDPP selected by the method
+     * selectLonDPP, using the same criteria as that method. This method does all the work.
      * NOTE: method selectLonDPP exists to justify the algorithm of this method and for testing.
      */
-    private int selectZoomLevel(double lonDPP) {
-        int zoomLevel = MAX_ZOOM_LEVEL;
-        for (int zl = MAX_ZOOM_LEVEL; zl >= MIN_ZOOM_LEVEL; zl--) {
-            zoomLevel = LONDPP_AT_DEPTH[zl] <= lonDPP ? zl : zoomLevel;
+    private int selectDepth(double lonDPP) {
+        int depth = DEPTH_MAX;
+        for (int zl = DEPTH_MAX; zl >= DEPTH_MIN; zl--) {
+            depth = LONDPP_AT_DEPTH[zl] <= lonDPP ? zl : depth;
         }
-        return zoomLevel;
+        return depth;
+    }
+
+    /**
+     * Calculates the length in longitude units of a tile at the given depth (zoom level).
+     * @param depth is the image depth (zoom level of the image).
+     * @return the length in longitude units of a tile at the given depth.
+     */
+    private double tileLonLength(int depth) {
+        return (ROOT_LRLON - ROOT_ULLON) / numTilesAtDepth(depth);
+    }
+
+    /**
+     * Calculates the length in latitude units of a tile at a given depth (zoom level).
+     * @param depth is the image depth (zoom level of the image).
+     * @return the length in latitude units of a tile at the given depth.
+     */
+    private double tileLatLength(int depth) {
+        return (ROOT_ULLAT - ROOT_LRLAT) / numTilesAtDepth(depth);
+    }
+
+    /**
+     * Calculates the number of tiles covered by the bounding box of the root tile at the given
+     * depth.
+     * Every image depth (zoom level) has double tiles per side as the immediate lower depth.
+     * @return the number of tiles covered by the total bounding box at the given depth
+     */
+    private int numTilesAtDepth(int depth) {
+        return (int) pow(2, depth);
     }
 
 }
