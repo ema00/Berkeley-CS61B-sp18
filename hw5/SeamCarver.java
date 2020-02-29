44f574f6df513f13a03bbf0e5075d98959246c5f
Emanuel Aguirre <ema_a_@hotmail.com>
2020-02-29 09:19:17 -0300

    hw5, implemented energy calculation in SeamCarver

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -1,3 +1,7 @@
+import static java.lang.Math.pow;
+import java.awt.Color;
+
+import edu.princeton.cs.algs4.Picture;
 
 
 
@@ -13,7 +17,7 @@ public class SeamCarver {
 
 
     public SeamCarver(Picture picture) {
-
+        this.picture = picture;
     }
 
 
@@ -21,7 +25,6 @@ public class SeamCarver {
      * Current picture, on which to perform seam carving.
      */
     public Picture picture() {
-        // TODO
         return this.picture;
     }
 
@@ -29,24 +32,67 @@ public class SeamCarver {
      * Width of current picture.
      */
     public int width() {
-        // TODO
-        return 0;
+        return picture.width();
     }
 
     /**
      * Height of current picture.
      */
     public int height() {
-        // TODO
-        return 0;
+        return picture.height();
     }
 
     /**
-     * Energy of pixel at column x and row y.
+     * Energy gradient of pixel at column x and row y. It is calculated as the sum of the energy
+     * gradients in the x and y directions.
      */
     public double energy(int x, int y) {
-        // TODO
-        return 0.0;
+        if (x < 0 || width() < x) {
+            throw new IndexOutOfBoundsException("Pixel is out of bound in the x direction.");
+        }
+        if(y < 0 || height() < y) {
+            throw new IndexOutOfBoundsException("Pixel is out of bound in the y direction.");
+
+        }
+        int width = width();
+        int height = height();
+        double xGradient = energyGradientX(x, y, width);
+        double yGradient = energyGradientY(x, y, height);
+        return xGradient + yGradient;
+    }
+
+    /**
+     * Calculates the energy gradient for a pixel, in the x direction.
+     * Rx(x,y)^2 + Gx(x,y)^2 + Bx(x,y)^2, where Rx, Gx and Bx are the differences in red, green
+     * and blue between the pixels at x-1 and x+1. For pixels that are on the limits (0 and W-1),
+     * W-1 is used in the case of 0, and 0 is used in the case of W-1 (it is wrapped around).
+     */
+    private double energyGradientX(int x, int y, int width) {
+        int x1 = (x - 1 + width) % width;
+        int x2 = (x + 1 + width) % width;
+        Color color1 = picture.get(x1, y);
+        Color color2 = picture.get(x2, y);
+        int deltaRed = color2.getRed() - color1.getRed();
+        int deltaGreen = color2.getGreen() - color1.getGreen();
+        int deltaBlue = color2.getBlue() - color1.getBlue();
+        return pow(deltaRed, 2) + pow(deltaGreen, 2) + pow(deltaBlue, 2);
+    }
+
+    /**
+     * Calculates the energy gradient for a pixel, in the y direction.
+     * Ry(x,y)^2 + Gy(x,y)^2 + By(x,y)^2, where Ry, Gy and By are the differences in red, green
+     * and blue between the pixels at y-1 and y+1. For pixels that are on the limits (0 and H-1),
+     * H-1 is used in the case of 0, and 0 is used in the case of H-1 (it is wrapped around).
+     */
+    private double energyGradientY(int x, int y, int height) {
+        int y1 = (y - 1 + height) % height;
+        int y2 = (y + 1 + height) % height;
+        Color color1 = picture.get(x, y1);
+        Color color2 = picture.get(x, y2);
+        int deltaRed = color2.getRed() - color1.getRed();
+        int deltaGreen = color2.getGreen() - color1.getGreen();
+        int deltaBlue = color2.getBlue() - color1.getBlue();
+        return pow(deltaRed, 2) + pow(deltaGreen, 2) + pow(deltaBlue, 2);
     }
 
     /**
